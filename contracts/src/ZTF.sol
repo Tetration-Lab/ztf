913e97678bc766c851f7533b495ecac2b699ce82
yoyoismee <yoyoismee@gmail.com>
2023-10-17 15:35:54 +0700

    update ztf.sol

--- contracts/src/ZTF.sol
+++ contracts/src/ZTF.sol
@@ -27,19 +27,25 @@ contract ZTF {
         bytes seal;
     }
 
+    uint numBounty = 0;
+    uint numClaimed = 0;
+
+    uint public lastBounty = 0;
+    uint public lastSecondaryCallback = 0;
     mapping(uint => Bounty) public bountyList;
     mapping(uint => secondaryCallback) public secondaryCallbacks;
     bytes32 public immutable IMG_ID;
-    address public constant riskZeroVerifier = 0x000000000000000000000000000000000000dEaD;
+    address public immutable RISK_ZERO_VERIFIER;
 
-    constructor(bytes32 imgID) {
+    constructor(bytes32 imgID, address verifier) {
         IMG_ID = imgID;
+        RISK_ZERO_VERIFIER = verifier;
     }
 
     // create a new bounty.
     function newBounty(address flag, address callback) external payable {
-        uint id = uint(keccak256(abi.encodePacked(msg.sender, flag)));
-        require(bountyList[id].owner == address(0), "Bounty already exists");
+        uint id = lastBounty + 1;
+        lastBounty = id;
         bountyList[id] = Bounty({
             bounty: msg.value,
             flag: flag,
@@ -47,6 +53,7 @@ contract ZTF {
             callback: callback,
             claimed: false
         });
+        numBounty += 1;
     }
 
     // add bounty to an existing bounty
@@ -57,16 +64,17 @@ contract ZTF {
         );
         bountyList[bountyID].bounty += msg.value;
     }
-    
+
     // add secondary callback to a bounty. can be claim after the bounty is claimed
     function addSecondaryCallback(
         address callback,
         uint targetBounty
     ) external payable {
-        uint id = uint(keccak256(abi.encodePacked(msg.sender, callback)));
+        uint id = lastSecondaryCallback + 1;
+        lastSecondaryCallback = id;
         require(
-            secondaryCallbacks[id].callback == address(0),
-            "Secondary callback already exists"
+            bountyList[targetBounty].owner != address(0),
+            "Bounty does not exist"
         );
         secondaryCallbacks[id] = secondaryCallback({
             callback: callback,
@@ -75,12 +83,10 @@ contract ZTF {
             claimed: false
         });
     }
+
     // capture flag with a valid proof
-    function claim(
-        uint bountyID,
-        ZClaim memory claimData
-    ) external {
-        IRiscZeroVerifier verifier = IRiscZeroVerifier(riskZeroVerifier);
+    function claim(uint bountyID, ZClaim memory claimData) external {
+        IRiscZeroVerifier verifier = IRiscZeroVerifier(RISK_ZERO_VERIFIER);
         // check
         require(
             verifier.verify(
@@ -108,18 +114,22 @@ contract ZTF {
         payable(claimData.claimer).transfer(totalBounty);
 
         // callback
-        ICallback(bountyList[bountyID].callback).callback(bountyList[bountyID].flag);
+        ICallback(bountyList[bountyID].callback).callback(
+            bountyList[bountyID].flag
+        );
+        numClaimed += 1;
     }
 
     // trigger list of secondary callbacks if bounty is claimed
-    function triggerCallback(uint[] memory targets) external{
-        for(uint i = 0; i < targets.length; i++){
+    function triggerCallback(uint[] memory targets) external {
+        for (uint i = 0; i < targets.length; i++) {
             require(
                 secondaryCallbacks[targets[i]].callback != address(0),
                 "Secondary callback does not exist"
             );
             require(
-                bountyList[secondaryCallbacks[targets[i]].targetBounty].claimed == true,
+                bountyList[secondaryCallbacks[targets[i]].targetBounty]
+                    .claimed == true,
                 "Target bounty not claimed"
             );
             require(
@@ -127,8 +137,10 @@ contract ZTF {
                 "Secondary callback already claimed"
             );
             secondaryCallbacks[targets[i]].claimed = true;
-            ICallback(secondaryCallbacks[targets[i]].callback).callback(bountyList[secondaryCallbacks[targets[i]].targetBounty].flag);
+            ICallback(secondaryCallbacks[targets[i]].callback).callback(
+                bountyList[secondaryCallbacks[targets[i]].targetBounty].flag
+            );
             payable(msg.sender).transfer(secondaryCallbacks[targets[i]].bonus);
         }
-    } 
+    }
 }
