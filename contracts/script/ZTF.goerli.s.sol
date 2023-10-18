2fe8db64d2873be6422f059d9838722676b0f415
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-10-18 16:24:29 +0700

    feat: add weth support for goerli

--- contracts/script/ZTF.goerli.s.sol
+++ contracts/script/ZTF.goerli.s.sol
@@ -17,12 +17,13 @@ contract ZTFScript is Script {
             ControlID.CONTROL_ID_1
         );
 
-        address[] memory assets = new address[](1);
-        assets[0] = 0xD8134205b0328F5676aaeFb3B2a0DC15f4029d8C;
+        address[] memory assets = new address[](2);
+        assets[0] = 0xD8134205b0328F5676aaeFb3B2a0DC15f4029d8C; // sDAI
+        assets[1] = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6; // wETH
         ZTF ztf = new ZTF(
             0xd914d2c3b3e85e88d0ea677aec9b284bb82353011ad428f3c6d9f0ec50d7a673,
             address(verifier),
-            assets // sDAI
+            assets
         );
 
         console2.log(address(ztf));
